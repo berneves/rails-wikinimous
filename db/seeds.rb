6143c926b5ab6d39057ce8b2bf5f575c5fdeee9d
Bernardo Neves <bernardosbneves@gmail.com>
2020-10-10 17:01:23 -0300

    wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  new_article = Article.create(title:
+Faker::FunnyName.name_with_initial ,
+ content: Faker::Quote.famous_last_words)
+end
